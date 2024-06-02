9b22ec122dcbc0819bd7f397cd7e9783a1fbaf17
naito <yunosuke.github@gmail.com>
2024-06-02 09:50:46 +0900

    feat : コメントリアクション機能

--- app/views/comment.py
+++ app/views/comment.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api
 from app.resources.comment import CommentResource
+from app.resources.comment_reaction import CommentReactionResource
 
 comment_bp = Blueprint('comment', __name__)
 comment_api = Api(comment_bp)
 
 comment_api.add_resource(CommentResource, '/comments', '/comments/<int:comment_id>')
+comment_api.add_resource(CommentReactionResource, '/comments/<int:comment_id>/<string:action>')
